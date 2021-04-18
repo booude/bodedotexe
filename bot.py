a8006483ddea9b87a967d4d2beb14e8b57cd188e
booude <booude@gmail.com>
2021-04-18 05:54:26 -0300

    Deletar comando e pylint

--- bot.py
+++ bot.py
@@ -41,11 +41,6 @@ client = Client(
 async def event_ready():
     print(f"{BOT_NICK} ta online!")
 
-@bot.event
-async def event_message(ctx):
-    if ctx.author.name.lower() == BOT_NICK.lower():
-        return
-    await bot.handle_commands(ctx)
 
 @bot.event
 async def event_message(ctx):
@@ -56,34 +51,56 @@ async def event_message(ctx):
     message = ctx.content
     if message[0] == BOT_PREFIX:
         cmd = message.split(' ')[0][1:]
-        if cmd == 'comando':
+        if cmd == 'comando' or cmd == 'command' or cmd == 'cmd':
             if(ctx.author.is_mod) or (ctx.author == CHANNEL):
-                message = message.replace('!comando', '').strip()
+                message = message.replace(f'!{cmd}', '').strip()
                 new = message.split()[0]
-                if new == 'novo':
+                if new == 'novo' or new == 'new' or new == 'add' or new == 'edit' or new == 'editar':
                     cmd = message.split()[1]
                     cmd2 = unidecode.unidecode(cmd.lower())
-                    dsc = message.replace(f'novo {cmd}','').strip()
-                    data={cmd2: dsc}
+                    dsc = message.replace(f'{new} {cmd}', '').strip()
+                    data = {cmd2: dsc}
                     add_command(data)
                     await ctx.channel.send_me(f'{ctx.author.name} -> Comando criado/editado com sucesso :D')
                     return
+                elif new == 'delete' or new == 'apaga' or new == 'del':
+                    cmd = message.split()[1]
+                    cmd2 = unidecode.unidecode(cmd.lower())
+                    if del_command(cmd2) != None:
+                        await ctx.channel.send_me(f'{ctx.author.name} -> Comando deletado :|')
+                        return
+                    else:
+                        await ctx.channel.send_me(f'{ctx.author.name} -> Comando "{cmd2}" não foi encontrado :\ ')
         else:
             msg = get_command(cmd.lower())
             await ctx.channel.send_me(f"{ctx.author.name} -> {msg}")
         return
+
 
 def get_command(cmd):
     with open(COMMAND_FILE) as json_file:
         command = json.load(json_file)
         return command[unidecode.unidecode(cmd)]
 
+
 def add_command(data):
     with open(COMMAND_FILE) as json_file:
         command = json.load(json_file)
         command.update(data)
     with open(COMMAND_FILE, 'w', encoding='utf-8') as json_file:
-        json.dump(command, json_file,ensure_ascii=False,indent=4,sort_keys=True)
+        json.dump(command, json_file, ensure_ascii=False,
+                  indent=4, sort_keys=True)
+
+
+def del_command(cmd):
+    with open(COMMAND_FILE) as json_file:
+        command = json.load(json_file)
+        boolean = command.pop(cmd, None)
+    with open(COMMAND_FILE, 'w', encoding='utf-8') as json_file:
+        json.dump(command, json_file, ensure_ascii=False,
+                  indent=5, sort_keys=True)
+    return boolean
+
 
 if __name__ == "__main__":
     bot.run()
